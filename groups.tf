3f7861d42da326e7de8dbef8ddc7aa30b35255ca
Iryna <zyavrusha@gmail.com>
2024-07-05 12:28:35 +0000

    add access key and secret key

--- groups.tf
+++ groups.tf
@@ -17,5 +17,5 @@ resource "aws_iam_user" "devopsusr1" {
 # add user to the group
 resource "aws_iam_user_group_membership" "user_devops_group" {
     user = aws_iam_user.devopsusr1.name
-    group = aws_iam_group.DevOps.name
+    groups = aws_iam_group.DevOps.name
 }
