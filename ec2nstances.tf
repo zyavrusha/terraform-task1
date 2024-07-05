415de17dcd4c9d397f9fcde529d1141952c6b23c
Iryna <zyavrusha@gmail.com>
2024-07-05 14:28:27 +0000

    fixing the typo in the ec2nstances.tf file

--- ec2nstances.tf
+++ ec2nstances.tf
@@ -1,7 +1,7 @@
 
 # create  simple ec2 instance for AWS 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-00402f0bdf4996822"
   instance_type = "t2.micro"
 
   tags = {
--- provider.tf
+++ provider.tf
@@ -10,7 +10,5 @@ terraform {
 }
 
 provider "aws" {
-    access_key = "AWS_ACCESS_KEY_ID"
-    secret_key = "AWS_SECRET_ACCESS_KEY"
     region  = "us-east-1"
 }
